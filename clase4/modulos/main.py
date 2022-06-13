eb4fe34e9c7c9f89bc1aff8bc494701ee3318b70
sara <hola@sara.dev>
2022-06-13 13:31:30 -0500

    Ejemplo de importación global

--- clase4/modulos/main.py
+++ clase4/modulos/main.py
@@ -2,7 +2,13 @@
 #saludos.saludo()
 #print (saludos.nombre)
 
-from saludos import saludo_nombre
+#from saludos import saludo_nombre
+#
+#saludo_nombre("Sarai")
+#saludo_nombre("Pecas")
 
-saludo_nombre("Sarai")
+from saludos import *
+
+saludo()
 saludo_nombre("Pecas")
+saludo_nombre_completo("Sara", "Falcon")
