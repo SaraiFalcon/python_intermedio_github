b49fe6fcc79f2c2af827cbf0768a389961661ad0
sara <hola@sara.dev>
2022-06-13 13:20:35 -0500

    Create saludos.py
    Se creó

---
+++ clase4/modulos/saludos.py
@@ -0,0 +1,10 @@
+def saludo ():
+    print("Hola")
+
+def saludo_nombre(nombre):
+    print(f"hola {nombre}")
+
+def saludo_nombre_completo (nombre, apellido_paterno, apellido_materno=''):
+    print(f"hola {nombre} {apellido_paterno} {apellido_materno}")
+    
+nombre = "nombre"
