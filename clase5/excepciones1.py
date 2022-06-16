6c908b66f389455034ce5597f48ed32921e97bc7
sara <hola@sara.dev>
2022-06-16 12:51:57 -0500

    Ejemplo agregado de un ciclo

--- clase5/excepciones1.py
+++ clase5/excepciones1.py
@@ -8,3 +8,11 @@ except ValueError as ex:
     print("Hubo un error de valor:", ex)
 except Exception as ex:
     print("Hubo una excepción:", ex)
+
+#Solicitar un número, si no es número manejar excepciones
+while True:
+     try:
+         x = int(input("Please enter a number: "))
+         break
+     except ValueError:
+         print("Oops!  That was no valid number.  Try again...")
