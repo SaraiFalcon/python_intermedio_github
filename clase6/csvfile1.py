3ca38f45b67f59f1ecd35f69a9fa34cafcb22ffc
sara <hola@sara.dev>
2022-06-17 14:25:05 -0500

    ejemplo de manipulación de archivos

--- clase6/csvfile1.py
+++
@@ -1,9 +0,0 @@
-#manipular archivos csv
-
-with open('console_games.csv', 'r') as csv_file:
-    print(csv_file.readline()) 
-
-    for line in csv_file.readlines():
-        print(line)
-    
-    #print(len(csv_file.readlines())) Solo sale cuando se comenta las 3 lineas de arriba
