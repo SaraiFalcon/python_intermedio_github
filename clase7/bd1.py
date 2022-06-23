95dc6ba22f8de6800dbcfcde7dabf296cea1ab36
sara <hola@sara.dev>
2022-06-23 15:02:38 -0500

    Mostrar todos los usuarios de la BD

--- clase7/bd1.py
+++ clase7/bd1.py
@@ -3,12 +3,18 @@
 import psycopg2
 
 conn = psycopg2.connect("dbname=python_intermedio_db user=postgres password=root")
-
 cur = conn.cursor()
 
+#Obtener usuario id 50
+cur.execute("SELECT * FROM usuarios where id=50;")
+print(cur.fetchone())
+
+#Obtener todos los usuarios como una tupla
 cur.execute("SELECT * FROM usuarios;")
 
-print(cur.fetchone())
+usuarios_tupla= cur.fetchall()
+for usuario in usuarios_tupla:
+    print(usuario)
 
 #Cierre de conexiones de BD
 cur.close()
