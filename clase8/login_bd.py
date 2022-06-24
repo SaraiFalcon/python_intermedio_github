4a6b74765298d469bf5cd75c6a4eb72e387d9b4b
sara <hola@sara.dev>
2022-06-24 11:52:55 -0500

    Continuar ejemplo de login

--- clase8/login_bd.py
+++ clase8/login_bd.py
@@ -5,8 +5,8 @@ conn = psycopg2.connect("dbname=python_intermedio_db user=postgres password=root
 #abrimos un cursos a la BD
 cur = conn.cursor()
 
-#user= shudleston5
-#password=XeL8MY
+#user = shudleston5
+#password = XeL8MY
 #fetchone() metodo que obtiene un registro de la base de datos
 #fetchall() metodo que obtiene todos los registros
 #rowcount contamos registros
@@ -21,11 +21,16 @@ consulta = f"SELECT username, password FROM usuarios where username = \'{usuario
 #Para ejecutar el codigo SQL
 cur.execute(consulta)
 
-#Mostrar el resultado de la consulta
-usuario_valido = cur.fetchall()
-print(usuario_valido)
+#Mostrar el resultado de la consulta. Retorna una tupla con un solo resultado
+usuario_encontrado = cur.fetchone()
+print(usuario_encontrado)
 
 #Si se encuentra un usuario con esos datos enviar un mensaje de bienvenida
+#Si encuentra algo en la tupla o sea, que sea mayor que 0 
+if len(usuario_encontrado) > 0:
+    print(f"Bienvenido {usuario_encontrado[0]}") # [0] Porque sólo quiero que imprima el nombre del usuario
+else:
+    print("Usuario o password incorrecto, intente de nuevo")
 
 #Si el
 
